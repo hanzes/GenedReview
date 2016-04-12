998108e1d9a947a19f169428a774c0ee6d9c6599
hanzes <za_cena@hotmail.com>
2016-04-12 15:40:22 +0700

    update test mongo

--- mongo.php
+++ mongo.php
@@ -1,23 +1,22 @@
 <?php
 $m = new MongoClient();
-var_dump($m);
+
 $db = $m->gendb;
-var_dump($db);
-$coll = $db->Gened;
-var_dump($coll);
-$coll->insert(array(
-    'key1' => 'Another Row',
-    'AnArray' => array(
-        'embedded array value 1', 
-        'embedded array value 2'
-    ),
-    'embeddedDoc1' => array(
-        'embedDoc1Key1' => 'Embedded text in Doc1',
-        'embedDoc1Key2' => 'More text for fun'
-    )
-    ));
-    echo '<h2 style="color:red">Below is our Document</h2>';
-$myDoc = $coll->findOne(array('key1' => 'Another Row'));
-print_r($myDoc);
-echo '</pre>';
+
+$coll = $db->reviews;
+
+/*$review = array( "title" => "human re",
+"id" => 2 );
+$coll->insert($review);
+
+$coll->update(array("title"=>"subject"),
+array('$set'=>array("title"=>"updated subject")));*/
+$str = "human";
+$create = "/" . $str . "/i";
+$regex = new MongoRegex($create);
+$cursor = $coll->find(array('title' => $regex));
+
+foreach ($cursor as $doc) {
+	print_r($doc['title']);
+}
 ?>
