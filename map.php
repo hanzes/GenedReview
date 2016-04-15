9cb2ae23dcfb6d0aaf0c57e257c01a1e9e62cf11
hanzes <za_cena@hotmail.com>
2016-04-15 14:55:28 +0700

    map & rank page by ICE :dancer:

--- map.php
+++ map.php
@@ -41,6 +41,10 @@
         </ul>
       </div>
     </div>
+
+	<!DOCTYPE PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+
     <div id="site_content">
       <div id="sidebar_container">
         <br><br><br><br>
@@ -50,21 +54,117 @@
         <h3>Find your class room</h3>
         <h4 style="font-size:120%;">How to use</h4>
             <p>1. input your subject id<br> 
-                2. click search </p>
+                2. click search <br></p>
         </div>   
       </div>
       <div id="content">
         <!-- insert the page content here -->
         <h1>Map</h1> 
         <br><br> 
+
+		<html>
+ <head>
+  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script> 
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script> 
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+  <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>  
+ </head>
+
+</html>
         <div id="map" style="width:600px;height:400px"></div>
-          <script>
-              var mapCanvas = document.getElementById("map");
-              var mapOptions = {
-                center: new google.maps.LatLng(13.7383, 100.5322), zoom: 16
-              }
-              var map = new google.maps.Map(mapCanvas, mapOptions);
-          </script>
+          <body>
+    <input id="pac-input" class="controls" type="text"
+        placeholder="Enter a location">
+    <div id="type-selector" class="controls">
+      <input type="radio" name="type" id="changetype-all" checked="checked">
+      <label for="changetype-all">All</label>
+
+     
+    </div>
+    <div id="map"></div>
+
+    <script>
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 13.7355, lng: 100.5313},
+          zoom: 16
+        });
+        var input = /** @type {!HTMLInputElement} */(
+            document.getElementById('pac-input'));
+
+        var types = document.getElementById('type-selector');
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+          }));
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(map, marker);
+        });
+
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+        function setupClickListener(id, types) {
+          var radioButton = document.getElementById(id);
+          radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+          });
+        }
+
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZ6_4pGvErwygV2evUv_d__d2Gn-K2Oz8&libraries=places&callback=initMap"
+        async defer></script>
         <br>
         <br>  
       </div>
@@ -75,3 +175,6 @@
   </div>
 </body>
 </html>
+
+
+
--- rank.php
+++ rank.php
@@ -1,14 +1,112 @@
 <!DOCTYPE HTML>
 <html>
 <?php 
+
+function array_sort($array, $on, $order=SORT_ASC)
+{
+    $new_array = array();
+    $sortable_array = array();
+
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on) {
+                        $sortable_array[$k] = $v2;
+                    }
+                }
+            } else {
+                $sortable_array[$k] = $v;
+            }
+        }
+
+        switch ($order) {
+            case SORT_ASC:
+                asort($sortable_array);
+            break;
+            case SORT_DESC:
+                arsort($sortable_array);
+            break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+    }
+
+    return $new_array;
+}
+
 $uri = "mongodb://distdbpro:distdb555@ds023570.mlab.com:23570/distdata";
 $m = new MongoClient($uri);
-
 $db = $m->selectDB("distdata");
+$coll = $db->rate;
+$cursor = $coll->find(array('category' => "Society"));
+$cursor2 =  $coll->find(array('category' => "Science"));
+$cursor3 = $coll->find(array('category' => "Interdisciplinary"));
+$cursor4 = $coll->find(array('category' => "Human"));
+$i=0;
+foreach ($cursor as $doc) {
+	$sname = array(0 =>$doc['title'],1 => $doc['rate'], 2=>$doc['SID']);
+	if($i==0){
+	$arr = array(array($doc['title'], $doc['rate'], $doc['SID']));
+	}
+	else{
+	$arr = array_merge($arr,array($sname));
+	}
+	
+	$i++;
 
-$coll = $db->reviews;
+}
+$i=0;
+foreach ($cursor2 as $doc) {
+	$sname2 = array(0 =>$doc['title'],1 => $doc['rate'], 2=>$doc['SID']);
+	if($i==0){
+	$arr2 = array(array($doc['title'], $doc['rate'], $doc['SID']));
+	}
+	else{
+	$arr2 = array_merge($arr2,array($sname));
+	}
+	
+	$i++;
+
+}
+$i=0;
+foreach ($cursor3 as $doc) {
+	$sname3 = array(0 =>$doc['title'],1 => $doc['rate'], 2=>$doc['SID']);
+	if($i==0){
+	$arr3 = array(array($doc['title'], $doc['rate'], $doc['SID']));
+	}
+	else{
+	$arr3 = array_merge($arr3,array($sname));
+	}
+	
+	$i++;
+
+}
+$i=0;
+foreach ($cursor4 as $doc) {
+	$sname4 = array(0 =>$doc['title'],1 => $doc['rate'], 2=>$doc['SID']);
+	if($i==0){
+	$arr4 = array(array($doc['title'], $doc['rate'], $doc['SID']));
+	}
+	else{
+	$arr4 = array_merge($arr4,array($sname));
+	}
+	
+	$i++;
+
+}
+
+
+
+$arrsoc = array_sort($arr,'rate',SORT_ASC);
+$arrsci = array_sort($arr,'rate',SORT_ASC);
+$arrin = array_sort($arr,'rate',SORT_ASC);
+$arrhum = array_sort($arr,'rate',SORT_ASC);
+
+
 ?>
-
 <head>
   <title>Gened Reviews</title>
   <meta name="description" content="website description" />
@@ -48,51 +146,132 @@ $coll = $db->reviews;
         </ul>
       </div>
     </div>
-    <div id="site_content">
-      <div id="sidebar_container">
-          <br><br><br><br>
-        <img class="paperclip" src="style/paperclip.png" alt="paperclip" />
-        <div class="sidebar">
-        <!-- insert your sidebar items here -->
-        <h3>Gened ranking table</h3>
-        <h4 style="font-size:120%;">How to use</h4>
-        <p>1. nothing</p>
-        </div>
-        
-      </div>
-      <div id="content">
-        <!-- insert the page content here -->
-        <h1>Rank</h1>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>RANK</th>
-            </tr>
-            <tr>
-                <td>2301379</td>
-                <td>TC</td>
-                <td>1</td>
-            </tr>
-            <tr>
-                <td>2304104</td>
-                <td>Physic2</td>
-                <td>2</td>
-            </tr>
-            <tr>
-                <td>2301463</td>
-                <td>Distdata</td>
-                <td>3</td>
-            </tr>
-            <tr>
-                <td>2301350</td>
-                <td>UI</td>
-                <td>4</td>
-            </tr>
-          </table>  
-       
-      </div>
-    </div>
+   <!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Price</title>
+    
+    
+    
+    
+        <link rel="stylesheet" href="css/style.css">
+
+    
+    
+    
+  </head>
+
+  <body>
+
+    <html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Document</title>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css">
+</head>
+<body>
+<section class="container">
+  <div class="row white">
+				<div class="block">
+
+					<div class="col-xs-12 col-sm-6 col-md-3">
+							<ul class="pricing p-green">
+								<li>
+									<img src="http://bread.pp.ua/n/settings_g.svg" alt="">
+									<big>Science</big>
+								</li>
+								<li>Responsive Design</li>
+								<li>Color Customization</li>
+								<li>HTML5 & CSS3</li>
+								<li>Styled elements</li>
+								<li>
+									<h3>Top</h3>
+									<span>per month</span>
+								</li>
+								<li>
+									<button>Join Now</button>
+								</li>
+							</ul>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-3">
+							<ul class="pricing p-yel">
+								<li>
+									<img src="http://bread.pp.ua/n/settings_y.svg" alt="">
+									<big>Interdisciplinary</big>
+								</li>
+								<li>Responsive Design</li>
+								<li>Color Customization</li>
+								<li>HTML5 & CSS3</li>
+								<li>Styled elements</li>
+								<li>
+									<h3>5</h3>
+									<span>per month</span>
+								</li>
+								<li>
+									<button>Join Now</button>
+								</li>
+							</ul>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-3">
+							<ul class="pricing p-red">
+								<li>
+									<img src="http://bread.pp.ua/n/settings_r.svg" alt="">
+									<big>Human</big>
+								</li>
+								<li>Responsive Design</li>
+								<li>Color Customization</li>
+								<li>HTML5 & CSS3</li>
+								<li>Styled elements</li>
+								<li>
+									<h3>For</h3>
+									<span>per month</span>
+								</li>
+								<li>
+									<button>Join Now</button>
+								</li>
+							</ul>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-3">
+							<ul class="pricing p-blue">
+								<li>
+									<img src="http://bread.pp.ua/n/settings_b.svg" alt="">
+									<big>Society</big>
+								</li>
+								<li><?php echo $arrsoc[0][0]?></li>
+								<li>Color Customization</li>
+								<li>HTML5 & CSS3</li>
+								<li>Styled elements</li>
+								<li>
+									<h3>You</h3>
+									<span>per month</span>
+								</li>
+								<li>
+									<button>Join Now</button>
+								</li>
+							</ul>
+					</div>
+
+
+				</div><!-- /block -->
+			</div><!-- /row -->
+  </section>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    
+    
+    
+    
+  </body>
+</html>
+
     <div id="footer">
       <p> by ICE JOY SAII BOSS P &nbsp;&nbsp; #cscu21</p>
     </div>
